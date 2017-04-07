517d432c390f44ba38624f3eef4dbacdda78dc46
Christian Tschoepe <tschoepe.christian@gmail.com>
2017-04-07 04:45:24 -0500

    Improved look of sections, filled in details.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 angular
   .module('ResumeApp', ['ngMaterial', 'ngSanitize'])
   .controller('HeadSkillsCtrl', function($scope) {
-    $scope.skills = ["Front End", "Back End", "Full Stack", "HTML", "CSS", "JavaScript", "Python", "Ruby", "NodeJS", "Ruby on Rails", "OracleSQL", "MongoDB", "TypeScript", "Dart", "Android SDK", "Perl", "C"];
+    $scope.skills = ["Front End", "Back End", "Full Stack", "HTML", "CSS", "JavaScript", "Python", "Ruby", "NodeJS", "Ruby on Rails", "OracleSQL", "MongoDB", "TypeScript", "Dart", "CoffeeScript", "Sass", "Bootstrap", "Android SDK", "Perl", "C"];
   })
   .controller('ExperienceCtrl', function($scope) {
     $scope.jobs = [
@@ -14,7 +14,7 @@ angular
         description: "Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.",
         responsibilities: [
           "Implemented an internal Angular UI/UX best practices website to ensure internal code consistency among all of the products in the Solarwinds web portfolio.",
-          "<mark>HTML</mark> <mark>CSS</mark> <mark>JavaScript</mark> <mark>NodeJS</mark> <mark>AngularJS</mark> <mark>express.js</mark>"
+          "<mark>HTML</mark> <mark>CSS</mark> <mark>JavaScript</mark> <mark>NodeJS</mark> <mark>AngularJS</mark> <mark>Express.js</mark>"
         ]
       },
       {
@@ -22,16 +22,15 @@ angular
         position: "Web App Development Intern", // TODO: Change title to something else, if possible
         responsibilities: [
           "Implemented a web interface MongoDB replacement for an outdated OracleSQL internal tool, significantly reducing software-as-a-service fees while adding critical permission management features for over 1000 employees.",
-          "<mark>HTML</mark> <mark>CSS</mark> <mark>JavaScript</mark> <mark>NodeJS</mark> <mark>MongoDB</mark> <mark>OracleSQL</mark> <mark>AngularJS</mark> <mark>express.js</mark>"
+          "<mark>HTML</mark> <mark>CSS</mark> <mark>JavaScript</mark> <mark>NodeJS</mark> <mark>MongoDB</mark> <mark>OracleSQL</mark> <mark>AngularJS</mark> <mark>Express.js</mark> <mark>Bootstrap</mark>"
         ]
       },
       {
         company: "Educational Design Studios",
         position: "Web App Developer",
-        description: "Led a small team to design and implement a Ruby on Rails web application to provide teachers with an easy way to obtain and share educational resources on various subjects.",
         responsibilities: [
-          "Developed a way for teachers to rate and preview educational materials, ensuring teachers obtain the best possible educational resources for students.",
-          "<mark>HTML</mark> <mark>CSS</mark> <mark>JavaScript</mark> <mark>Ruby</mark> <mark>Ruby on Rails</mark>"
+          "Led a small team to design and implement a web application to provide teachers with an easy way exchange educational resources on various subjects.",
+          "<mark>HTML</mark> <mark>CSS</mark> <mark>JavaScript</mark> <mark>Ruby</mark> <mark>Ruby on Rails</mark> <mark>Bootstrap</mark>"
         ]
       },
       {
@@ -39,7 +38,7 @@ angular
         position: "Web App Development Intern",
         responsibilities: [
           "Collaborated in a team environment, creating a Ruby web-based application process to streamline the creation and requesting of internships among employees and admins.",
-          "<mark>HTML</mark> <mark>CSS</mark> <mark>JavaScript</mark> <mark>Ruby</mark>"
+          "<mark>HTML</mark> <mark>CSS</mark> <mark>JavaScript</mark> <mark>Ruby</mark> <mark>Bootstrap</mark>"
         ]
       }
     ];
@@ -48,8 +47,8 @@ angular
     $scope.projects = [
       {
         title: "todolist.social",
-        description: "Website that does cool stuff",
-        technologies: ["HTML", "CSS", "JavaScript", "AngularJS", "NodeJS"]
+        description: "A todo list application hosted at todolistsocial.herokuapp.com that allows users to keep track of and share their goals with friends, utilizing peer accountability as motivation for users to complete their goals.",
+        technologies: ["HTML", "CSS", "JavaScript", "AngularJS", "NodeJS", "Express.js", "Bootstrap"]
       },
       {
         title: "PintOS",
@@ -62,15 +61,15 @@ angular
     $scope.skillsets = [
       {
         title: "Programming Languages",
-        skills: ["JavaScript", "TypeScript", "Java", "Python", "Ruby"]
+        skills: ["JavaScript", "Java", "Python", "Ruby", "CoffeeScript", "TypeScript", "Dart", "C", "Perl"]
       },
       {
         title: "Web Front-End",
-        skills: ["HTML", "CSS", "JavaScript"]
+        skills: ["HTML", "CSS", "JavaScript", "TypeScript", "CoffeeScript", "Dart", "AngularJS", "Angular 2", "jQuery", "Bootstrap", "Material Design"]
       },
       {
         title: "Web Back-End",
-        skills: ["NodeJS"]
+        skills: ["Express.js", "Ruby on Rails"]
       }
     ];
   })
