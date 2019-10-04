9eb0995a5f2d7f45f49ca6d210c81635fff998b3
Christian Tschoepe <tschoepe.christian@gmail.com>
2019-10-04 17:39:42 +0300

    Format content and document for better printability

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ document.querySelector("#printButton").onclick = () => {
       position: "Full-stack Developer",
       responsibilities: [
         "Owned the company UI component library (React, TypeScript) in close collaboration with product designers.",
-        "Interviewed engineering candidates in the technical round on a survey of front-end, back-end and general technical software skills.",
-        "Implemented the company's video and image management system integrated across our product (Rails, TypeScript), allowing customers to quickly upload, preview, and reuse assets in ads and our image and video editors.",
-        "Developed the text engine in our web-based video editor and renderer."
+        "Implemented our video and image management system integrated across our product (Rails, TypeScript), allowing customers to quickly upload, preview, and reuse assets in ads and our image and video editors.",
+        "Developed the text engine in our web-based video editor and renderer.",
+        "Interviewed engineering candidates on a survey of front-end, back-end and general technical software skills."
       ],
       technologies: [
         "TypeScript",
